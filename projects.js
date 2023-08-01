6ba5acf01da00a6f1d145c2c462babc1a1168176
Flávio Lucas <flaviolucas389@gmail.com>
2023-08-01 14:30:21 -0300

    fix(responsivity): improved responsivity in site

---
+++ projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    name: 'Sama - Ecossistema de ensino',
+    description: 'Esse foi um dos primeiros e mais longos projetos de que participei. Se trata de uma plataforma onde os professores de ensino infantil e fundamental podem disponibilizar conteúdos para seus alunos e também avisos para os pais dessas crianças.',
+    participation: 'Minha participação foi, principalmente no desenvolvimento do ',
+    images: [
+      'projects/sama/sama-1'
+    ]
+  }
+];
+
+(() => {
+  console.log('teste');
+})()
